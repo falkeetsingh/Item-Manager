de809cf781e274075fba9010c1b61aff3b65836a
falkeetsingh <147339485+falkeetsingh@users.noreply.github.com>
2025-06-21 16:02:31 +0530

    fixed typo

--- server/server.js
+++ server/server.js
@@ -12,28 +12,29 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-// Static uploads path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/items', itemRoutes);
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
 if (process.env.NODE_ENV === 'production') {
-  
-    const frontendBuildPath = path.resolve(__dirname, '..', 'dist');
-    
-    app.use(express.static(frontendBuildPath));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(frontendBuildPath, 'index.html'));
-    });
-  }
+  const frontendBuildPath = path.resolve(__dirname, '..', 'dist');
   
+  app.use(express.static(frontendBuildPath));
 
-app.listen(PORT,()=>{
-    console.log("server is running on 5000");
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendBuildPath, 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+});
